7a956f3fb9540edcdd4c69bc224ae8f862845f50
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2022-12-02 10:04:44 +0700

    add file login

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,23 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:goumkm/pages/landing_umkm/login.dart';
 import '../../theme.dart';
 import 'berita_umkm/berita_umkm_page.dart';
 import 'profil_umkm/profil_umkm_page.dart';
 import 'review_umkm/review_umkm_page.dart';
 
-
-class HomePage extends StatefulWidget  {
-  const HomePage({Key? key,}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int pageIndex=0;
+  int pageIndex = 0;
   @override
   void initState() {
     super.initState();
@@ -27,28 +29,44 @@ class _HomePageState extends State<HomePage> {
       ReviewUmkmPage(),
       BeritaUmkmPage(),
       ProfilUmkmPage(),
+      LoginPage(),
     ];
     return Scaffold(
       body: page[pageIndex],
       extendBody: true,
       bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor:Colors.transparent,
+        backgroundColor: Colors.transparent,
         color: whiteLightColor,
         items: <Widget>[
-          Icon(Icons.rate_review_outlined,color: greenColor,size: 30,),
-          Icon(Icons.newspaper, size: 30,color: greenColor,),
-          Icon(Icons.person, size: 30,color: greenColor,),
+          Icon(
+            Icons.rate_review_outlined,
+            color: greenColor,
+            size: 30,
+          ),
+          Icon(
+            Icons.newspaper,
+            size: 30,
+            color: greenColor,
+          ),
+          Icon(
+            Icons.person,
+            size: 30,
+            color: greenColor,
+          ),
+          Icon(
+            Icons.login_outlined,
+            size: 30,
+            color: greenColor,
+          ),
         ],
         animationCurve: Curves.easeInOut,
         animationDuration: Duration(milliseconds: 500),
         onTap: (index) {
           setState(() {
-            pageIndex=index;
+            pageIndex = index;
           });
         },
       ),
     );
   }
-
 }
-
