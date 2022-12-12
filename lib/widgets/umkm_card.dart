7696980c04ffadd4100a8a032fbeb72e824985bd
ihzadfm <112409398+ihzadfm@users.noreply.github.com>
2022-12-12 20:00:07 +0700

    update umkm card

--- lib/widgets/umkm_card.dart
+++ lib/widgets/umkm_card.dart
@@ -3,9 +3,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:goumkm/models/review_umkm_model_new.dart';
+import 'package:goumkm/models/review_umkm_model.dart';
 import 'package:goumkm/models/umkm_model.dart';
 import 'package:goumkm/pages/review_umkm/detail_review_umkm_page.dart';
+import 'package:goumkm/pages/review_umkm/rate_umkm_page.dart';
 import 'package:goumkm/widgets/rating_item.dart';
 
 import '../theme.dart';
@@ -17,17 +18,17 @@ class UmkmCard extends StatelessWidget{
   final double rating;
   final int index;
   final int lastIndex;
-  const UmkmCard({Key? key, required this.umkmModel, required this.rating, required this.index, required this.lastIndex,}) : super(key: key);
+  final String role;
+  const UmkmCard({Key? key, required this.umkmModel, required this.rating, required this.index, required this.lastIndex, required this.role,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return GestureDetector(
       onTap: () async {
-        List<ReviewUmkmModelNew> listReview=[];
+        List<ReviewUmkmModel> listReview=[];
         var jsonMap =await GoUmkmProvider().getReviewUMKM();
         for(int i=0; i <jsonMap.length;i++){
-          ReviewUmkmModelNew umkmModel = ReviewUmkmModelNew.fromJson(jsonMap[i]);
+          ReviewUmkmModel umkmModel = ReviewUmkmModel.fromJson(jsonMap[i]);
           listReview.add(umkmModel);
         }
         Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailReviewUmkmPage(listReviewUmkm: listReview.where((id) => id.fields!.umkm==umkmModel.pk).toList(), umkmModel: umkmModel, rating: rating.toStringAsFixed(1),
@@ -78,23 +79,48 @@ class UmkmCard extends StatelessWidget{
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                      child: Text(umkmModel.fields!.nama!,style: TextStyle(fontSize: 20.sp,fontWeight: FontWeight.w700,color: whiteLightColor),maxLines: 1,)),
-                  SizedBox(height: 10.h,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       SizedBox(
                           width:200.w,
-                          child: Text(umkmModel.fields!.pemilik!,style: TextStyle(fontSize: 16.sp,fontWeight: FontWeight.w500,color: whiteLightColor),maxLines: 2,)),
+                          child: Text(umkmModel.fields!.nama!,style: whiteLightTextStyle.copyWith(fontSize: 20.sp,fontWeight: FontWeight.w700,),maxLines: 2,)),
                       Row(
                         children: [
-                          Text("${rating.toStringAsFixed(1)}/5",style: TextStyle(fontSize: 16.sp,fontWeight: FontWeight.w500,color: whiteLightColor),maxLines: 2,),
+                          Text("${rating==0?0:rating.toStringAsFixed(1)}/5",style: whiteLightTextStyle.copyWith(fontSize: 16.sp,),maxLines: 2,),
                           SizedBox(width: 10.w,),
                           Image.asset(
                             "assets/star_yellow.png",width: 16.w,)
                         ],
-                      )
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10.h,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                          width:160.w,
+                          child: Text(umkmModel.fields!.pemilik!,style: whiteLightTextStyle.copyWith(fontSize: 16.sp,),maxLines: 2,)),
+                      Visibility(
+                        visible: role=="K"?true:false,
+                        child: TextButton(
+                          onPressed: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => RateUmkm(umkmModel: umkmModel,)));
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: greenColor,
+                            shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                          ),
+                          child:Row(
+                            children: [
+                              Text("Rate Now",style: whiteLightTextStyle.copyWith(fontSize: 14.sp,fontWeight: FontWeight.w700,),maxLines: 1,),
+                              SizedBox(width: 10.w,),
+                              Icon(Icons.star_rate,color: Colors.orange,size: 20,)
+                            ],
+                          ),
+                        ),
+                      ),
                     ],
                   )
                 ],
