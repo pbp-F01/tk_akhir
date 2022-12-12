c2f6680265aa74e1e4e9e3b80ae82991bf8e90fd
ihzadfm <112409398+ihzadfm@users.noreply.github.com>
2022-12-12 19:58:51 +0700

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-Color blackColor = const Color(0xff263238);
-Color blackLightColor = const Color(0xcc000000);
+Color blackColor=const Color(0xff000000);
 Color whiteColor = const Color(0xffFFFFFF);
-Color greyColor = const Color(0xff455A64);
-Color greyLightColor = const Color(0xFFD9D9D9);
-Color greyLightColorDesc = const Color(0xffA1A1A1);
-Color greenColor = const Color(0xFF4E6C50);
-Color darkGreenColor = const Color(0xFF395144);
-Color brownColor = const Color(0xFFAA8B56);
-Color whiteLightColor = const Color(0xFFF0EBCE);
+Color greenColor=const Color(0xFF4E6C50);
+Color darkGreenColor=const Color(0xFF395144);
+Color whiteLightColor=const Color(0xFFF0EBCE);
+
+
+TextStyle blackTextStyle = GoogleFonts.inter(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.inter(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+TextStyle whiteLightTextStyle = GoogleFonts.inter(
+  fontWeight: FontWeight.w500,
+  color: whiteLightColor,
+);
+TextStyle greenTextStyle = GoogleFonts.inter(
+  fontWeight: FontWeight.w500,
+  color: greenColor,
+);
