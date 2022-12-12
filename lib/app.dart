1521f5aeadc78130b9e3a968173aa4b522dc03fa
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2022-12-12 09:59:32 +0700

    add login register

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:goumkm/pages/home_page.dart';
+import 'package:goumkm/pages/landing_umkm/login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:goumkm/pages/landing_umkm/register.dart';
 import 'package:goumkm/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -10,11 +17,25 @@ class MyApp extends StatelessWidget {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return ScreenUtilInit(
         minTextAdapt: true,
-        builder: (context , child) {
-          return MaterialApp(
-            home: HomePage(),
-          );
-        }
-        );
+        builder: (context, child) {
+          return Provider(
+              create: (_) {
+                CookieRequest request = CookieRequest();
+                return request;
+              },
+              child: MaterialApp(
+                title: 'Goumkm',
+                theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                ),
+                initialRoute: '/home',
+                routes: {
+                  '/home': (context) => const HomePage(),
+                  '/login': (context) => const LoginPage(),
+                  '/register': (context) => const Register(),
+                },
+                home: HomePage(),
+              ));
+        });
   }
 }
